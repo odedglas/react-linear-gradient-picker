af7a1e4a6a1b164c41611862e59aa5dd0c258b9d
odeadglaz <oded.goldglas+git@fiverr.com>
2020-04-28 08:03:08 +0300

    init.

---
+++ src/components/hooks/useDragging/constants.js
@@ -0,0 +1,5 @@
+/**
+ * The touch start event, used to distinguish touch / mouse actions
+ * @type {String}
+ */
+export const TOUCH_START_EVENT = 'touchstart';
--- src/components/hooks/useDragging/index.js
+++ src/components/hooks/useDragging/index.js
@@ -1,21 +1,47 @@
 import { useState, useEffect } from 'react';
 import { noop } from '../../../lib';
+import { TOUCH_START_EVENT } from './constants';
+
+const DRAG_HANLDERS = {
+	MOUSE: {
+		stop: (e) => e.preventDefault() && e.stopPropagation(),
+		coordinates: ({ clientX, clientY }) => ({ clientX, clientY }),
+		dragEvent: { name: 'mousemove' },
+		dragEndEvent: { name: 'mouseup' }
+	},
+	TOUCH: {
+		stop: noop,
+		coordinates: (e) => {
+			const [touch] = e.touches;
+			return { clientX: touch.pageX, clientY: touch.pageY };
+		},
+		dragEvent: { name: 'touchmove', options: { cancelable: true, passive: true } },
+		dragEndEvent: { name: 'touchend' }
+	}
+};
+
+const isTouch = (e) => e.type === TOUCH_START_EVENT;
 
 const useDragging = ({ onDragStart = noop, onDrag, onDragEnd = noop }) => {
 	const [context, setContext] = useState({});
+	const [handler, setHandler] = useState();
 	const [dragging, setDragging] = useState(false);
 
 	const handleMouseDown = (e) => {
-		e.preventDefault();
-		e.stopPropagation();
+		const handler = isTouch(e) ? DRAG_HANLDERS.TOUCH : DRAG_HANLDERS.MOUSE;
+		console.log('Drag start', handler);
+		handler.stop(e);
 
-		if (!e.button) activate(e);
+		if (!e.button) activate(e, handler);
 	};
 
-	const activate = (e) => {
+	const activate = (e, handler) => {
 		setDragging(true);
+		handler && setHandler(handler);
 
-		onDragStart(e);
+		const coordinates = handler ? handler.coordinates(e) : e;
+
+		onDragStart(coordinates);
 	};
 
 	const deactivate = () => {
@@ -28,21 +54,25 @@ const useDragging = ({ onDragStart = noop, onDrag, onDragEnd = noop }) => {
 	const handleDrag = (e) => {
 		if (!dragging) return;
 
-		context.change = onDrag(e);
+		context.change = onDrag(handler.coordinates(e));
 	};
 
 	useEffect(() => {
-		if (dragging) {
+		if (!handler) return;
 
-			document.addEventListener('mousemove', handleDrag);
-			document.addEventListener('mouseup', deactivate);
+		const { dragEvent, dragEndEvent } = handler;
+
+		if (dragging) {
+			console.log('Bindiing:', handler)
+			document.addEventListener(dragEvent.name, handleDrag, dragEndEvent.options);
+			document.addEventListener(dragEndEvent.name, deactivate);
 		}
 
 		return () => {
-			document.removeEventListener('mousemove', handleDrag);
-			document.removeEventListener('mouseup', deactivate);
+			document.removeEventListener(dragEvent.name, handleDrag);
+			document.removeEventListener(dragEndEvent.name, deactivate);
 		};
-	}, [dragging]);
+	}, [dragging, handler]);
 
 	return [
 		handleMouseDown,
