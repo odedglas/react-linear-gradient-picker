c018f3e9465d949484a7550606a9e028f2473619
odeadglaz <oded.goldglas+git@fiverr.com>
2022-05-29 22:53:06 +0300

    Fix doubleclick issue.

--- src/components/ColorStop/index.js
+++ src/components/ColorStop/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { noop } from '../../lib';
 import { STOP_PROP_TYPES } from '../propTypes';
 import useStopDragging from './hooks/useStopDragging';
@@ -6,6 +6,7 @@ import './index.scss';
 
 const ColorStop = ({ stop, limits, onPosChange, onDeleteColor, onDragStart = noop, onDragEnd = noop}) => {
 	const colorStopRef = useRef();
+	const [allowRemoveOnDoubleClick, setAllowRemoveOnDoubleClick] = useState(false)
 	const [drag] = useStopDragging({
 		stop,
 		limits,
@@ -16,6 +17,10 @@ const ColorStop = ({ stop, limits, onPosChange, onDeleteColor, onDragStart = noo
 		colorStopRef
 	});
 
+	useEffect(() => {
+		setTimeout(() => setAllowRemoveOnDoubleClick(true), 1000);
+	}, []);
+
 	const { offset, color, isActive, opacity } = stop;
 
 	return (
@@ -23,7 +28,9 @@ const ColorStop = ({ stop, limits, onPosChange, onDeleteColor, onDragStart = noo
 			ref={colorStopRef}
 			style={{ left: offset }}
 			onMouseDown={drag}
-			onDoubleClick={() => onDeleteColor(stop.id)}
+			onDoubleClick={() => {
+				allowRemoveOnDoubleClick && onDeleteColor(stop.id)
+			}}
 			onTouchStart={drag}>
 			<div style={{ backgroundColor: color, opacity }}/>
 		</div>
