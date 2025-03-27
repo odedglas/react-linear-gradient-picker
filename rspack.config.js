956cabbd4d3e1b395d6e918efc0e484db3a03cca
encodedz <odedgo@monday.com>
2025-03-27 17:46:04 +0200

    Rspack instead of webpack

---
+++ rspack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const { rspack } = require('@rspack/core');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  devtool: 'source-map',
+  externals: {
+    react: 'react',
+  },
+  target: 'web',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+    library: {
+      type: 'commonjs2',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jp(e*)g)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'builtin:swc-loader',
+          options: {
+            jsc: {
+              parser: {
+                syntax: 'ecmascript',
+                jsx: true,
+              },
+              transform: {
+                react: {
+                  runtime: 'automatic',
+                  development: false,
+                  refresh: false,
+                },
+              },
+              externalHelpers: true,
+            },
+            env: {
+              targets: 'Chrome >= 48',
+            },
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new rspack.SwcJsMinimizerRspackPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+}; 
