fb314fe67bbc61e719fa408b954352a330350509
encodedz <odedgo@monday.com>
2025-03-29 22:35:47 +0300

    Exports.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import AnglePicker from './components/AnglePicker';
 import GradientPicker from './components/GradientPicker';
-import GradientPickerPopover, { GRADIENT_TYPES } from './components/GradientPickerPopover';
+import GradientPickerPopover from './components/GradientPickerPopover';
 import { getGradientPreview, asBackground, angleToGradientCords } from './lib';
 
-export { GradientPicker, GradientPickerPopover, AnglePicker, getGradientPreview, GRADIENT_TYPES, asBackground, angleToGradientCords };
+export * from './types';
+export { GradientPicker, GradientPickerPopover, AnglePicker, getGradientPreview, asBackground, angleToGradientCords };
